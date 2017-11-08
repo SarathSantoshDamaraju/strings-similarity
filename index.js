0d4b5ae3d7f479bcf285deeb42a1f9fb4fd12219
Sarath Damaraju <sarathdamaraju@hotmail.com>
2017-11-08 22:14:01 +0530

    Returns json

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const city_names = require('./city_names');
 function main(city, count=1){
     return((typeof(city) === "string")&&(typeof(count) === "number")&&(count>=0) ? wrapper(city,city_names, count) : -1 )
 }
-module.exports = {main};
+module.exports = main;
