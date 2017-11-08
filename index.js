6a7da4aa352135f3323b4da78d24f74648cf3a98
Sarath Damaraju <sarathdamaraju@hotmail.com>
2017-11-08 21:43:15 +0530

    Fixed Issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const city_names = require('./city_names');
 
 function main(p, city_names, count=1){
     p = p.toLowerCase();
-    return((typeof(p) === "string") && (Array.isArray(q) === true || typeof(q) === "string") ? wrapper(p,q, count) : "False" )
+    return((typeof(p) === "string") && (Array.isArray(city_names) === true || typeof(city_names) === "string") ? wrapper(p,city_names, count) : "False" )
 }
 module.exports = {main};
 
